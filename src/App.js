e6080d9d04beb0f8b3c8fd596da257d96150aa3b
Gruheeth <gruheeth.288@gmail.com>
2023-06-25 23:54:41 +0530

    Add balance component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
 import { Header } from './components/Header';
+import { Balance } from './components/Balance';
+import './App.css';
 
 function App() {
   return (
     <div>
       <Header />
+      <div className='container'>
+        <Balance />
+      </div>
     </div>
   );
 }
