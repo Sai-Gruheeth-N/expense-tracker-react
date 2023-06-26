478bc26df73a9870710073fff67b83726f06e66b
Gruheeth <gruheeth.288@gmail.com>
2023-06-26 14:06:52 +0530

    Implement delete transaction feature

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const Transaction = ({ transaction }) => {
     const sign = transaction.amount < 0 ? '-' : '+';
     const className = transaction.amount < 0 ? 'minus' : 'plus';
+    const { deleteTransaction } = useContext(GlobalContext);
     return (
         <li className={className}>
             {transaction.text}
             <span>{sign}${Math.abs(transaction.amount)}</span>
-            <button className='delete-btn'>X</button>
+            <button className='delete-btn' onClick={() => deleteTransaction(transaction.id)}>X</button>
         </li>
     )
 }
